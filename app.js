d270000aecfa4d7ca5436b8d007b18b040433e63
mohit gupta <mgmohithero@gmail.com>
2016-11-14 16:06:10 +0530

    my first angular work

---
+++ app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by mohit on 14-Nov-16.
+ */
+(function () {
+    'use strict';
+    
+    angular.module('myFirstApp',[])
+        .controller('myFirstController',function () {
+            
+        })
+})();
