75ad4705b0b93519e7720b0b3ef54fe93c547963
mohit gupta <mgmohithero@gmail.com>
2016-11-15 21:39:18 +0530

    name calc

--- app.js
+++ app.js
@@ -6,9 +6,22 @@
     
     angular.module('myFirstApp',[])
         .controller('myFirstController',function ($scope) {
-            $scope.name="mohit";
+            $scope.name="";
             $scope.hellofunction=function () {
                 return "hey this is a function ";
+            };
+            $scope.totalvalue=0;
+            $scope.calculate=function () {
+              var newval=changemyval($scope.name);
+             $scope.totalvalue=newval;   
+            };
+            function changemyval(mystr) {
+                var newtot=0;
+                for(var i=0;i<mystr.length;i++)
+                {
+                    newtot+=(mystr.charCodeAt(i));
+                }
+                return newtot;
             }
         })
 })();
